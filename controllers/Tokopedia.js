3111b4a5b58b8ccbcbdba54a84a3824ff9a2c83e
sidharta_prabawa <sidhartaprabawa@gmail.com>
2024-01-08 01:22:42 +0700

    added fz 532435 skip process 5

--- controllers/Tokopedia.js
+++ controllers/Tokopedia.js
@@ -84,13 +84,13 @@ exports.getOrderList = async (req, res) => {
   const fromTime = yesterdayDate.startOf('day').unix();
 
   // Calculate the end date (yesterday)
-  const endDate = currentDate.clone().subtract(1, 'day');
+  // const endDate = currentDate.clone().subtract(1, 'day');
 
   // Set the time to 23:59:59 for yesterday
-  // const toTime = endDate.endOf('day').unix();
+  const toTime = endDate.endOf('day').unix();
 
   // Set the time to 23:59:59 for yesterday
-  const toTime = yesterdayDate.endOf('day').unix();
+  // const toTime = yesterdayDate.endOf('day').unix();
   
   console.log('Unix timestamp for from_date (00:00):', fromTime);
   console.log('Unix timestamp for to_date (23:59):', toTime);
