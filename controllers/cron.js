aaa2a8b4774326df57313129d4eee5a380c43fc9
GENERALI\SidhartaP <sidharta.prabawa@generali.co.id>
2024-04-22 17:59:33 +0700

    tokped test 3

--- controllers/cron.js
+++ controllers/cron.js
@@ -84,7 +84,7 @@ const cronExpressions = [
 ];
 
 exports.startCron = async(req, res) => {
-    response.res200(res, "000", new Date()+"Cronjob Start "+new Date().toLocaleString(), {})
+    // response.res200(res, "000", new Date()+"Cronjob Start "+new Date().toLocaleString(), {})
 
     // Create and schedule cron jobs for the task with each cron expression
     for (let i = 0; i < cronExpressions.length; i++) {
