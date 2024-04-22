ddb36c932133c04814e7e300879594bcb02a3162
GENERALI\SidhartaP <sidharta.prabawa@generali.co.id>
2024-04-22 20:44:39 +0700

    test final cron time

--- controllers/Tokopedia.js
+++ controllers/Tokopedia.js
@@ -147,10 +147,11 @@ exports.getOrderListInternal = async (shopId) => {
   // const toTime = moment().unix();
 
   // Calculate the end date (yesterday)
-  const endDate = currentDate.clone().subtract(1, 'day');
+  // const endDate = currentDate.clone().subtract(1, 'day');
 
   // Set the time to 23:59:59 for yesterday
-  const toTime = endDate.endOf('day').unix();
+  // const toTime = endDate.endOf('day').unix();
+  const toTime = moment().unix();
 
   // Set the time to 23:59:59 for yesterday
   // const toTime = yesterdayDate.endOf('day').unix();
@@ -421,10 +422,11 @@ exports.getOrderList = async (req, res) => {
   const fromTime = yesterdayDate.startOf('day').unix();
 
   // Calculate the end date (yesterday)
-  const endDate = currentDate.clone().subtract(1, 'day');
+  // const endDate = currentDate.clone().subtract(1, 'day');
 
   // Set the time to 23:59:59 for yesterday
-  const toTime = endDate.endOf('day').unix();
+  // const toTime = endDate.endOf('day').unix();
+  const toTime = moment().unix();
 
   // Set the time to 23:59:59 for yesterday
   // const toTime = yesterdayDate.endOf('day').unix();
